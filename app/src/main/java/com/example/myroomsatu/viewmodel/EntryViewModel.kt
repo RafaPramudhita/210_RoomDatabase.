9c039da7e2782bfa546da1a467df8992feb0443e
RafaPramudhita <rafapram360@gmail.com>
2025-11-27 20:06:18 +0700

    Fungsi untuk memvalidasi input dan Berisi status Siswa saat ini

---
+++ app/src/main/java/com/example/myroomsatu/viewmodel/EntryViewModel.kt
@@ -0,0 +1,43 @@
+package com.example.myroomsatu.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.myroomsatu.repositori.RepositoriSiswa
+import com.example.myroomsatu.room.Siswa
+import kotlinx.coroutines.launch
+
+class EntryViewModel(
+    private val repositoriSiswa: RepositoriSiswa
+) : ViewModel() {
+
+    /**
+     * Berisi status Siswa saat ini.
+     */
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    /* Fungsi untuk memvalidasi input */
+    private fun validasiInput(
+        uiState: DetailSiswa = uiStateSiswa.detailSiswa
+    ): Boolean =
+        with(uiState) {
+            nama.isNotBlank() &&
+                    alamat.isNotBlank() &&
+                    telpon.isNotBlank()
+        }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa = UIStateSiswa(
+            detailSiswa = detailSiswa,
+            isEntryValid = validasiInput(detailSiswa)
+        )
+    }
+
+
+}
+
+
+
