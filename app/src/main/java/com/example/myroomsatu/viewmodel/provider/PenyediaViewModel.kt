8b41530fe18829d4895e498611fbb942bc8314b0
RafaPramudhita <rafapram360@gmail.com>
2025-11-27 20:22:17 +0700

    membuat PenyediaViewModel factory dan ekstensi aplikasiSiswa

---
+++ app/src/main/java/com/example/myroomsatu/viewmodel/provider/PenyediaViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.myroomsatu.viewmodel.provider
+
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.myroomsatu.repositori.AplikasiSiswa
+import com.example.myroomsatu.viewmodel.EntryViewModel
+import com.example.myroomsatu.viewmodel.HomeViewModel
+
+object PenyediaViewModel {
+
+    val Factory = viewModelFactory {
+        initializer {
+            val app = aplikasiSiswa()
+            HomeViewModel(repositoriSiswa = app.container.repositoriSiswa)
+        }
+        initializer {
+            val app = aplikasiSiswa()
+            EntryViewModel(repositoriSiswa = app.container.repositoriSiswa)
+        }
+    }
+}
+
