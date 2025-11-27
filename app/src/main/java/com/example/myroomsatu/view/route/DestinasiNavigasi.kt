16118e25a666dabf1591ce408454868114dd56d7
RafaPramudhita <rafapram360@gmail.com>
2025-11-27 20:25:43 +0700

    Nama unik untuk menentukan jalur untuk composable

--- app/src/main/java/com/example/myroomsatu/view/route/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/myroomsatu/view/route/DestinasiNavigasi.kt
@@ -1,5 +1,10 @@
 package com.example.myroomsatu.view.route
 
 interface DestinasiNavigasi {
+    /**
+     * Nama unik untuk menentukan jalur untuk composable.
+     */
+    val route: String
+
 
 }
