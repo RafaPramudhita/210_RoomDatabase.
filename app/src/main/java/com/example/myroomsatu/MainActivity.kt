e5ba72168a84bbc647b3d64e517ca1a3c219e445
RafaPramudhita <rafapram360@gmail.com>
2025-11-28 12:31:54 +0700

    menghubungkan SiswaApp sebagai konten utama di MainActivity

---
+++ app/src/main/java/com/example/myroomsatu/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.myroomsatu
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold   // <- ini yang benar
+import androidx.compose.ui.Modifier
+import com.example.myroomsatu.ui.theme.MyRoomSatuTheme
+import com.example.myroomsatu.view.uicontroller.SiswaApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyRoomSatuTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    SiswaApp(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
